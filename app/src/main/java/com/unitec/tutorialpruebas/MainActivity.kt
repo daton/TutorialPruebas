53c2f007c5b39d55bf4d03a48fbfd9c57d8b7db4
daton <rapidclimate@gmail.com>
2022-05-03 19:05:49 -0500

    Espresso

--- app/src/main/java/com/unitec/tutorialpruebas/MainActivity.kt
+++ app/src/main/java/com/unitec/tutorialpruebas/MainActivity.kt
@@ -3,13 +3,23 @@ package com.unitec.tutorialpruebas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        val  x=23
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        val v:(String, Int)->String ={cadena, valor->cadena+""+valor}
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.e("XXX", "Depurar esta parte ${MiLoginActivity.x}");
 
         findViewById<Button>(R.id.btn_mensajito).setOnClickListener {
             findViewById<TextView>(R.id.texto_actualizar).text="Oprimido"
--- app/src/main/java/com/unitec/tutorialpruebas/MiLoginActivity.kt
+++ app/src/main/java/com/unitec/tutorialpruebas/MiLoginActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MiLoginActivity : AppCompatActivity() {
+    companion object{
+        val x=33;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_login)
+
     }
 }
